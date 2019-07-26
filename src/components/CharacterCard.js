5292dda0905d4040ae14849bfdcc612328a33329
Kristin Barr <kristinbarr@gmail.com>
2019-07-26 11:42:50 -0400

    character card basic info display

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,20 @@
 import React from 'react'
+// import { Link } from 'react-router-dom'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard (props) {
+
+  console.log('card', props)
+  const {image, name, species, location, origin, episodes } = props.char
+
+  return (
+  <div>
+    <img src={image}></img>
+    <h3>{name}</h3>
+    <h6>{species}</h6>
+    <p>{location.name}</p>
+    <p>{origin.name}</p>
+    {/* <hr/> */}
+    <p>Episodes</p>
+  </div>
+  )
 }
